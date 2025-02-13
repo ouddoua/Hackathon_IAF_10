2ecfddef4799541ee5961f71fa0e3d40b0e11020
cortesmc <cacm@cassini.group>
2025-02-13 11:29:17 +0100

    chatbot implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import Chatbot from "./components/chatbot";
 import App from './App.tsx';
 import './index.css';
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Chatbot /> 
     </BrowserRouter>
   </StrictMode>
 );
